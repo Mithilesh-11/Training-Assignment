e0b2a6c9604f94e063c0da173604e583a279e78d
Mithilesh Watane <mithilesh.kanakasoftware@gmail.com>
2026-06-19 15:09:20 +0530

    Updated

--- Day2/activeUsersAge.js
+++ Day2/activeUsersAge.js
@@ -5,11 +5,18 @@ const users = [
   { id: 3, name: "Charlie", age: 35, active: true }
 ];
 
-
+// As Mentioned in the Assignment: Using filter, map, and reduce to calculate total age of active users
 const totalAge = users
                  .filter(user => user.active)
                  .map(user => user.age)
                  .reduce((sum, age) => sum + age, 0);
+
+
+// Alternate approach without chaining using single reduce method
+
+// const totalAge = users.reduce((sum, user) => {
+//     return user.active ? sum + user.age : sum;
+// }, 0);
 
 
 console.log("Total Age:", totalAge);
