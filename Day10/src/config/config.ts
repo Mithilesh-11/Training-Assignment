082a0856edbb8018ad4fcdfbe175dc7207598d3a
Mithilesh Watane <mithilesh.kanakasoftware@gmail.com>
2026-06-27 12:51:50 +0530

    Implementation of Day 10 Assignment

--- Day10/src/config/config.ts
+++ Day10/src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 const environment = process.env.NODE_ENV || "development";
 
-dotenv.config({path: `.env.${environment}`});
+dotenv.config({path: `.env`});
 export const config = {
   port: process.env.PORT || "3001",
   nodeEnv:process.env.NODE_ENV || "development",
